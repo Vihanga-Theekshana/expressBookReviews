d9758b27b485bc244bf5ce77a4b87ab8b85a671b
Vihanga-Theekshana <theekshanavihanga2@gmail.com>
2026-02-10 14:20:11 -0500

    Completed Tasks 10-13 with async axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,8 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require("axios");
+
 
 
 public_users.post("/register", (req,res) => {
@@ -31,6 +33,16 @@ public_users.get('/',function (req, res) {
   return res.send(JSON.stringify(books,null,4));
 });
 
+public_users.get('/async/books',async(req,res)=>{
+    try{
+        const response = await axios.get("http://localhost:5000/");
+        return res.status(200).json(response.data);
+    }catch(err){
+        return res.status(500).json({ message: "Error fetching books" });
+
+    }
+});
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
@@ -41,6 +53,16 @@ public_users.get('/isbn/:isbn',function (req, res) {
     }
     return res.status(404).json({ message: "Book not found" });
  });
+
+ public_users.get('/async/isbn/:isbn',async(req,res)=>{
+    try{
+        const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+        return res.status(200).json(response.data);
+
+    }catch(err){
+        return res.status(404).json({ message: "Book not found" });
+    }
+ })
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -55,6 +77,18 @@ public_users.get('/author/:author',function (req, res) {
 
 });
 
+public_users.get("/promise/author/:author",async(req,res)=>{
+    const author = req.params.author;
+
+    try{
+        const response = await axios.get(`http://localhost:5000/author/${encodeURIComponent(author)}`);
+          return res.status(200).json(response.data);
+    }catch(err){
+        return res.status(404).json({ message: "Book not found" });
+    }
+
+})
+
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
@@ -66,6 +100,7 @@ public_users.get('/title/:title',function (req, res) {
   }
   return res.status(404).json({ message: "Book not found" });
 });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
